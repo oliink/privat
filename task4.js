77606ccf9d03569c5e6a21f21a1a3575ce824044
oliink <65308796+oliink@users.noreply.github.com>
2020-08-29 11:36:54 +0300

    Update task4.js

--- task4.js
+++ task4.js
@@ -1,14 +1,24 @@
 // отобразить данные используя html и javascript
 
-async function getResponse() {
-    let response = await fetch('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3');
-    let content = await response.text();  //.json()
-    // console.log(content);
-    document.body.innerHTML += `<pre>${content}</pre>`;
-}
+// c помощью XMLHttpRequest()
 
-getResponse();
+let xhr = new XMLHttpRequest();
+let url = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3';
 
+xhr.open('GET', url); 
+xhr.send();
 
+xhr.onload = function() {
+        document.body.innerHTML = `<pre>${xhr.response}</pre>`;  
+  };
 
+// с помощью fetch()
 
+// async function getResponse() {
+//     let response = await fetch('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3');
+//     let content = await response.text();  //.json()
+//     // console.log(content);
+//     document.body.innerHTML = `<pre>${content}</pre>`;  //=
+// }
+
+// getResponse();
